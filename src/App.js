3ba81a1f977dd22ac4e162f5cd90f034d51e710f
Aniket Agrawal <aniket@abacus.ai>
2023-12-16 18:55:09 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import SignIn from './templates/SignIn';
 import SignUp from './templates/SignUp';
+import MainPage from './templates/MainPage';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<SignIn />} />
+                <Route path="/" element={<MainPage />} />
                 <Route path="/SignIn" element={<SignIn />} />
                 <Route path="/SignUp" element={<SignUp />} />
             </Routes>
