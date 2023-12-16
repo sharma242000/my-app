d142ba8a28b323b933a37e42a8f31ba0a0aa0668
sharma242000 <sharmamohit242000@gmail.com>
2023-12-16 18:42:00 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import SignIn from './templates/SignIn';
 import SignUp from './templates/SignUp';
+import { Chat } from './chat/Chat';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route path="/" element={<SignIn />} />
                 <Route path="/SignIn" element={<SignIn />} />
                 <Route path="/SignUp" element={<SignUp />} />
+                <Route path='/Chat' element = {<Chat />} />
             </Routes>
         </BrowserRouter>
     );
